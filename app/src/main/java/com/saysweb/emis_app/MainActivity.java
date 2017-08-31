aad2915e435bcb0f42b07203272886140b6d1fe6
sukant-saysweb <sagacious_sukant@yahoo.com>
2017-08-31 08:56:41 +0530

    Initial Commit

--- app/src/main/java/com/saysweb/emis_app/MainActivity.java
+++ app/src/main/java/com/saysweb/emis_app/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //--This is the main activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
